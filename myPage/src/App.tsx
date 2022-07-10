9b65c72e0a4df5080bf76b08801a3de906644d7d
Jeong yong hoon <jyh.dev@gmail.com>
2022-07-10 22:18:45 +0900

    THREEJS CAMERA TEST

--- myPage/src/App.tsx
+++ myPage/src/App.tsx
@@ -5,7 +5,7 @@ import {Canvas} from "@react-three/fiber";
 function App() {
     return (
         <div>
-            <Canvas>
+            <Canvas camera={{zoom: 100, position: [50, 50, 50]}}>
                 <ambientLight/>
                 <pointLight position={[10, 10, 10]}/>
                 <Box position={[-1.2, 0, 0]}/>
