cb1e48f78db42b4cad7a810e583ccf68eb375a02
Jeong yong hoon <jyh.dev@gmail.com>
2022-07-14 22:34:39 +0900

    CAMERA TEST

--- myPage/src/App.tsx
+++ myPage/src/App.tsx
@@ -3,16 +3,22 @@ import Box from "./component/Box";
 import {Stars} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 import Ground from "./component/Ground";
-import CustomCamera from "./component/CustomCamera";
 
 import {Physics} from "@react-three/cannon";
+import * as THREE from "three";
+import CustomCamera from "./component/CustomCamera";
 
 
 //https://www.youtube.com/watch?v=FGG0EeMNUl0
-function App() {
+const App = () => {
+
+    // const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 5, 100);
+    const camera = new THREE.OrthographicCamera(70, window.innerWidth / window.innerHeight, 5, 100);
+    camera.position.z = 5;
+
     return (
-        <Canvas>
-            <CustomCamera/>
+        <Canvas camera={camera}>
+            {/*<CustomCamera/>*/}
             <Stars/>
             <ambientLight intensity={0.5}/>
             <spotLight position={[10, 5, 10]} angle={0.3}/>
--- myPage/src/component/CustomCamera.tsx
+++ myPage/src/component/CustomCamera.tsx
@@ -1,9 +1,8 @@
-import * as THREE from 'three';
 import {OrbitControls} from "@react-three/drei";
 
 export default function CustomCamera() {
 
-    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 100, 1000);
+    // const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 100, 1000);
 
     return (
         <OrbitControls/>
