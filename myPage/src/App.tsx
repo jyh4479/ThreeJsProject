ca6d32945767c305cea475b4fd16817dde31f868
Jeong yong hoon <jyh.dev@gmail.com>
2022-07-14 22:27:02 +0900

    CAMERA TEST
    어렵다..

--- myPage/src/App.tsx
+++ myPage/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Box from "./component/Box";
-import {OrbitControls, Stars} from "@react-three/drei";
+import {Stars} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 import Ground from "./component/Ground";
+import CustomCamera from "./component/CustomCamera";
 
 import {Physics} from "@react-three/cannon";
+
 
 //https://www.youtube.com/watch?v=FGG0EeMNUl0
 function App() {
     return (
         <Canvas>
-            <OrbitControls/>
+            <CustomCamera/>
             <Stars/>
             <ambientLight intensity={0.5}/>
             <spotLight position={[10, 5, 10]} angle={0.3}/>
---
+++ myPage/src/component/CustomCamera.tsx
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+import {OrbitControls} from "@react-three/drei";
+
+export default function CustomCamera() {
+
+    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 100, 1000);
+
+    return (
+        <OrbitControls/>
+    );
+}
